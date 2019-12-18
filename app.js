e2afc92005676447d8b3626425a506997adb0254
Edwin Vargas <edwinleandrovargas@gmail.com>
2019-12-18 18:03:44 -0400

    Add CORS support

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express      from 'express';
 import path         from 'path';
 import cookieParser from 'cookie-parser';
 import logger       from 'morgan';
+import cors         from 'cors';
 
 import { api, index, users } from './routes';
 
@@ -13,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
