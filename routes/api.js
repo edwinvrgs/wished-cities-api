920f9dedf3e14d3a3a923d858daf8ed8d83f0bc8
Edwin Vargas <edwinleandrovargas@gmail.com>
2019-12-18 16:14:19 -0400

    Add static data

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,14 @@ router.get('/', function (req, res, next) {
   res.send('Hello world from API route');
 });
 
+router.get('/countries', function (req, res, next) {
+  // TODO - Get this values from database or a public API
+  res.send(['Venezuela', 'Chile', 'EEUU']);
+});
+
+router.get('/cities', function (req, res, next) {
+  // TODO - Get this values from database
+  res.send(['Caracas', 'Santiago', 'New York']);
+});
+
 export default router;
