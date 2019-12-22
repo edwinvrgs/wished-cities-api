1e85829b1312c2c92ff6f328111d59f56e0e0ceb
Edwin Vargas <edwinleandrovargas@gmail.com>
2019-12-22 18:43:05 -0400

    Remove unused features

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 import createError  from 'http-errors';
 import express      from 'express';
-import path         from 'path';
 import cookieParser from 'cookie-parser';
 import logger       from 'morgan';
 import cors         from 'cors';
 
-import { api, index, users } from './routes';
+import { api, index } from './routes';
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api', api);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -37,7 +30,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send();
 });
 
 module.exports = app;
