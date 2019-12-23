041fe2ad0f588320fcf7ef41a79f21f7326d607d
Edwin Vargas <edwinleandrovargas@gmail.com>
2019-12-23 00:53:26 -0400

    Add SQL scripts to the project

---
+++ database/scripts/country.sql
@@ -0,0 +1,4 @@
+INSERT INTO country (id, name) VALUES (1, 'Venezuela');
+INSERT INTO country (id, name) VALUES (2, 'Chile');
+INSERT INTO country (id, name) VALUES (3, 'EEUU');
+INSERT INTO country (id, name) VALUES (4, 'Colombia');
