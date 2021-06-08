26b91e5966dc86ad24a612b210e8df06c679a393
Edwin Vargas <edwinleandrovargas@gmail.com>
2021-06-08 13:01:08 -0400

    Update index.js

--- database/config/index.js
+++ database/config/index.js
@@ -11,14 +11,14 @@ const defaultOptions = {
 module.exports = {
   development: {
     ...defaultOptions,
-    use_env_variable: process.env.DEV_DATABASE_URL,
+    url: process.env.DEV_DATABASE_URL,
   },
   test: {
     ...defaultOptions,
-    use_env_variable: process.env.TEST_DATABASE_URL,
+    url: process.env.TEST_DATABASE_URL,
   },
   production: {
     ...defaultOptions,
-    use_env_variable: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 };
